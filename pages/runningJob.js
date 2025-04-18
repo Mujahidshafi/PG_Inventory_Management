d34f9edbce4c970b96006f826069a1c519b3214d
p-maina <puritymaina@csus.edu>
2025-04-17 19:29:16 -0700

    Update runningJob.js
    added columns

--- pages/runningJob.js
+++ pages/runningJob.js
@@ -5,7 +5,16 @@ import Layout from "../components/layout";
 function RunningJob() {
   return (
     <Layout title="Running Job">
-      <span>Running Job</span>
+      <div className= "w-screen h-[100%] flex flex-col gap-5">
+        <div className= "h-16 bg-gray-100 text-white flex items-center px-6 rounded-2xl">
+        </div>
+  
+        <div className= "flex flex-auto gap-10 p-6" >
+          <div className = "flex-1 bg-gray-100 rounded-2xl"></div>
+          <div className= "flex-1 bg-gray-100 rounded-2xl"></div>
+        </div>
+      </div>
+  
     </Layout>
   );
 }
