48fb6e2a4d96a6750748326c62bbd62ffb86ee25
p-maina <puritymaina@csus.edu>
2025-04-20 15:46:45 -0700

    correction from merge

--- pages/runningJob.js
+++ pages/runningJob.js
@@ -2,16 +2,9 @@ import React from "react";
 //import "../App.css";
 import Layout from "../components/layout";
 import Button from "@/components/button";
-import Layout from "../components/layout";
-import Button from "@/components/button";
 
 function RunningJob() {
   const jobMenu = [
-    { title: "Lot Number", value: "24J1" },
-    { title: "Process ID", value: "2870" },
-    { title: "Product Description", value: "YPC" },
-    { title: "Location", value: "HQ-6" },
-    { title: "Amount", value: "50,000 lbs" },
     { title: "Lot Number", value: "24J1" },
     { title: "Process ID", value: "2870" },
     { title: "Product Description", value: "YPC" },
@@ -24,11 +17,7 @@ function RunningJob() {
       <div className="w-screen h-[100%] flex flex-col gap-5">
         <div className="h-16 bg-gray-100 text-white flex items-center px-6 rounded-2xl">
           <div className="grid grid-cols-5 gap-1 items-center justify-items-center w-full">
-      <div className="w-screen h-[100%] flex flex-col gap-5">
-        <div className="h-16 bg-gray-100 text-white flex items-center px-6 rounded-2xl">
-          <div className="grid grid-cols-5 gap-1 items-center justify-items-center w-full">
             {jobMenu.map((job, index) => (
-              <div key={index} className="flex flex-col ">
               <div key={index} className="flex flex-col ">
                 <span className="text-black font-semibold"> {job.title} </span>
                 <span className="text-black"> {job.value} </span>
@@ -38,12 +27,7 @@ function RunningJob() {
         </div>
 
         <div className="flex flex-auto gap-10 p-6">
-
-        <div className="flex flex-auto gap-10 p-6">
           {/*Panel 1*/}
-          <div className="flex-1 bg-gray-100 rounded-2xl p-4">
-            <div className="w-full flex-1 w-[85%] grid grid-cols-2 bg-gray-300 text-black rounded-2xl p-2">
-              <div className="flex items-center justify-center"> Box 1 </div>
           <div className="flex-1 bg-gray-100 rounded-2xl p-4">
             <div className="w-full flex-1 w-[85%] grid grid-cols-2 bg-gray-300 text-black rounded-2xl p-2">
               <div className="flex items-center justify-center"> Box 1 </div>
@@ -62,28 +46,8 @@ function RunningJob() {
                   required
                 />
               </div>
-                <label
-                  htmlFor="Weight"
-                  className="flex items-center justify-center"
-                >
-                  Weight
-                </label>
-                <input
-                  type="text"
-                  id="Weight"
-                  className="w-auto bg-gray-50 border-gray-300 text-black text-sm rounded-lg px-2 py-1"
-                  placeholder="LB's"
-                  required
-                />
-              </div>
             </div>
           </div>
-
-          <div className="flex-1 flex flex-col gap-4 bg-gray-100 rounded-2xl p-4">
-            {/*Panel 2*/}
-            <div className="flex-1 bg-gray-100 rounded-2xl p-4">
-              {/*Output Display*/}
-              <div className="overflow-x-auto bg-gray-100 rounded-md text-sm text-center">
 
           <div className="flex-1 flex flex-col gap-4 bg-gray-100 rounded-2xl p-4">
             {/*Panel 2*/}
